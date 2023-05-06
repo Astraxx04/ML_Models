0cf6f95ee3f51d4542a862efc331e449b6fea61b
Astraxx04 <gagan200254@gmail.com>
2023-05-06 16:13:00 +0530

    New Files

--- OpenCV/read.py
+++ OpenCV/read.py
@@ -1,7 +1,16 @@
 import cv2 as cv
+#Image
+# img = cv.imread('Photos/cat.jpg')
+# cv.imshow('Cat', img)
+# cv.waitKey(0)
 
-img = cv.imread('Photos/cat.jpg')
+#Video
+capture = cv.VideoCapture('Videos/dog.mp4')
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
 
-cv.imshow('Cat', img)
-
-cv.waitKey(0)
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyallWindows()
